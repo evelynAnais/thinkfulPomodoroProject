fa81c3f70b5531fb7c1cb4ebb7733b9b3c3ca2d7
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-06-17 22:13:13 -0600

    broke out display into its own component

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -3,6 +3,7 @@ import useInterval from "../utils/useInterval";
 import FocusControl from "./FocusControl";
 import BreakControl from "./BreakControl";
 import TimerControl from "./TimerControl";
+import TimerDisplay from "./TimerDisplay";
 // These functions are defined outside of the component to insure they do not have access to state
 // and are, therefore more likely to be pure.
 
@@ -105,35 +106,7 @@ function Pomodoro() {
         <TimerControl isTimerRunning={isTimerRunning}
           playPause={playPause} 
         />
-      <div>
-        {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
-        <div className="row mb-2">
-          <div className="col">
-            {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
-            <h2 data-testid="session-title">
-              {session?.label} for 25:00 minutes
-            </h2>
-            {/* TODO: Update message below correctly format the time remaining in the current session */}
-            <p className="lead" data-testid="session-sub-title">
-              {session?.timeRemaining} remaining
-            </p>
-          </div>
-        </div>
-        <div className="row mb-2">
-          <div className="col">
-            <div className="progress" style={{ height: "20px" }}>
-              <div
-                className="progress-bar"
-                role="progressbar"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                aria-valuenow="0" // TODO: Increase aria-valuenow as elapsed time increases
-                style={{ width: "0%" }} // TODO: Increase width % as elapsed time increases
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+        <TimerDisplay session={session} />
     </div>
   );
 }
